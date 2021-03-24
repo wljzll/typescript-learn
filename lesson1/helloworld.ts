249fc9b6c5179a00d0ebb13e6d1ee528e0e17953
1826865187@qq.com <1826865187@qq.com>
2021-03-24 17:29:53 +0800

    '基本数据类型'

---
+++ lesson1/helloworld.ts
@@ -0,0 +1,9 @@
+// 布尔类型(boolean)
+let married: boolean = false;
+// 数字类型(number)
+let age: number = 10;
+// 字符串类型(string)
+let firstname: string = 'zfpx';
+// 数组类型(array)
+let arr2: number[] = [4, 5, 6];
+let arr3: Array<number> = [7, 8, 9];
